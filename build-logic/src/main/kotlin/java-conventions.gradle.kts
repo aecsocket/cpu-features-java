53e2fa937db6b9f689607ef023f93ff7c6391a0e
aecsocket <aecsocket@tutanota.com>
2023-02-09 21:53:33 +0000

    More build scripts

--- build-logic/src/main/kotlin/java-conventions.gradle.kts
+++ build-logic/src/main/kotlin/java-conventions.gradle.kts
@@ -12,6 +12,5 @@ indra {
 }
 
 repositories {
-    mavenLocal()
     mavenCentral()
 }
--- cpu-features-jni-natives-macos-arm64/build.gradle.kts
+++ cpu-features-jni-natives-macos-arm64/build.gradle.kts
@@ -9,6 +9,21 @@ dependencies {
 }
 
 tasks {
+    register<Exec>("generateNativesMacos") {
+        group = "natives"
+        val buildDir = "$rootDir/cpu_features/"
+
+        workingDir = File(buildDir)
+        commandLine = listOf("cmake", "-S.", "-Bbuild", "-DBUILD_TESTING=OFF", "-DCMAKE_BUILD_TYPE=Release")
+
+        doLast {
+            exec {
+                workingDir = File(buildDir)
+                commandLine = listOf("cmake", "--build", "build", "--config", "Release", "-j")
+            }
+        }
+    }
+
     jar {
         val jarTask = this
         bindings.tasks.withType<LinkSharedLibrary> {
