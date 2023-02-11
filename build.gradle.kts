b2d74c864c6b08eb1954d79f0db3952c1fe44406
aecsocket <aecsocket@tutanota.com>
2023-02-11 21:12:21 +0000

    Try another way of passing version type

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ val nativesMacos      = projects.cpuFeaturesJniNativesMacos.dependencyProject
 val nativesMacosArm64 = projects.cpuFeaturesJniNativesMacosArm64.dependencyProject
 
 group = "io.github.aecsocket"
-version = "1.0.0"
+version = "1.0.0-SNAPSHOT"
 description = "Java bindings for Google cpu_features"
 
 tasks.register("printVersionType") {
