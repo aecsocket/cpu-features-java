924c9e5e2b2a3ae82926dae2eef1f76f83d6023b
aecsocket <aecsocket@tutanota.com>
2023-03-08 18:24:39 +0000

    Attempt 6

--- build-logic/src/main/kotlin/java-conventions.gradle.kts
+++ build-logic/src/main/kotlin/java-conventions.gradle.kts
@@ -1,10 +1,7 @@
-import gradle.kotlin.dsl.accessors._a3cef13561c8f096ac9de7f8d52a5c05.signing
-
 plugins {
     id("base-conventions")
     id("java-library")
     id("net.kyori.indra")
-    signing apply false
 }
 
 indra {
@@ -15,15 +12,6 @@ indra {
 
 repositories {
     mavenCentral()
-}
-
-signing {
-    val signingKey = findProperty("signingKey") as? String
-    val signingPassword = findProperty("signingPassword") as? String
-    if (signingKey != null) {
-        println("${project.name}: Signing with in-memory PGP keys")
-        useInMemoryPgpKeys(signingKey, signingPassword)
-    }
 }
 
 tasks {
