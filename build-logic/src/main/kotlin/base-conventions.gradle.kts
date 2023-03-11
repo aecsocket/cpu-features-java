7e003eb4bd8dea28855a140cdf90e709c95e439d
aecsocket <aecsocket@tutanota.com>
2023-03-11 16:51:04 +0000

    Move signing back to publishing

--- build-logic/src/main/kotlin/base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/base-conventions.gradle.kts
@@ -1,17 +1,7 @@
 plugins {
     id("net.kyori.indra.git")
-    signing apply false
 }
 
 group = rootProject.group
 version = rootProject.version
 description = rootProject.description
-
-extensions.findByType<SigningExtension>()?.apply {
-    val signingKey = findProperty("signingKey") as? String
-    val signingPassword = findProperty("signingPassword") as? String
-    if (signingKey != null) {
-        println("${project.name}: Signing with in-memory PGP keys")
-        useInMemoryPgpKeys(signingKey, signingPassword)
-    }
-}
