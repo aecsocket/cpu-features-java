89637604dd44a7785500edb541b7528a4739b374
aecsocket <aecsocket@tutanota.com>
2023-03-28 19:05:41 +0100

    hello

--- build-logic/src/main/kotlin/base-conventions.gradle.kts
+++ build-logic/src/main/kotlin/base-conventions.gradle.kts
@@ -5,3 +5,14 @@ plugins {
 group = rootProject.group
 version = rootProject.version
 description = rootProject.description
+
+afterEvaluate {
+    extensions.findByType<SigningExtension>()?.apply {
+        val signingKey = findProperty("signingKey") as? String
+        val signingPassword = findProperty("signingPassword") as? String
+        if (signingKey != null) {
+            println("${project.name}: Signing with in-memory PGP keys")
+            useInMemoryPgpKeys(signingKey, signingPassword)
+        }
+    }
+}
